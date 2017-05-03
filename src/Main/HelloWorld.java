1421004cec11878388808f3a489ea3d06c88f5bd
Nirsberger <fn63kuca@dom.uni-leipzig.de>
2017-05-03 10:43:47 +0200

    added even more super crazy turbo stuff

--- src/Main/HelloWorld.java
+++ src/Main/HelloWorld.java
@@ -3,9 +3,9 @@ package Main;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO lalalalalalala
 		
-		System.out.println("Hello World!") ;
+		System.out.println("Hello Leipzig!") ;
 
 	}
 
